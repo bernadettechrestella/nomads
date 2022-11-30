28b836ebe71630b72733726a359d50183cf1f2da
bernadettechrestella <bernadettechrestella@gmail.com>
2022-11-30 17:11:30 +0900

    Update Active

--- resources/views/includes/admin/sidebar.blade.php
+++ resources/views/includes/admin/sidebar.blade.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
         <div class="sidebar-brand-text mx-3">Nomads Admin</div>
     </a>
 
@@ -10,17 +10,17 @@
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-tachometer-alt"></i>
+    <li class="nav-item {{(request()->is('admin')) ? 'active' : ''}}">
+        <a class="nav-link" href="{{ route('dashboard') }}">
+            <i class=" fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
 
     <hr class="sidebar-divider">
 
-    <li class="nav-item">
-        <a class="nav-link" href="index.html">
-            <i class="fas fa-fw fa-hotel"></i>
+    <li class="nav-item {{(request()->is('admin/travel-packages')) ? 'active' : ''}}">
+        <a class="nav-link" href="{{ route('travel-packages.index') }}">
+            <i class=" fas fa-fw fa-hotel"></i>
             <span>Paket Travel</span></a>
     </li>
 
