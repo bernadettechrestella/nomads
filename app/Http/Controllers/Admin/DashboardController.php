7cf942c028314230a972026161ef2e6eb2d3b9b4
bernadettechrestella <bernadettechrestella@gmail.com>
2022-12-03 13:37:52 +0900

    Update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Transactions;
+use App\Models\TravelPackages;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        return view('pages.admin.dashboard', [
+            'travel_packages' => TravelPackages::count(),
+            'transactions' => Transactions::count(),
+            'transactions_pending' => Transactions::where('transactions_status', 'PENDING')->count(),
+            'transactions_success' => Transactions::where('transactions_status', 'SUCCESS')->count(),
+        ]);
     }
 }
