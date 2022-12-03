f6ce56f1be83e580b79007f0004922e316e15927
bernadettechrestella <bernadettechrestella@gmail.com>
2022-12-03 13:48:55 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TravelPackages;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.home');
+        $items = TravelPackages::with(['galleries'])->get();
+        return view('pages.home', [
+            'items' => $items
+        ]);
     }
 }
